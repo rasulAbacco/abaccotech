ea779676b9a7b8c10e663cb83cbee80fa3026da2
rasulAbacco <rasul.abacco@gmail.com>
2025-11-13 23:03:57 +0530

    Fix import paths for services in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,12 @@ import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 // import Services from "./pages/Services";
-import WebsiteDesigning from "./pages/services/WebsiteDesigning";
-import CRMDevelopment from "./pages/services/CRMDevelopment";
-import AppDevelopment from "./pages/services/AppDevelopment";
-import CloudManagement from "./pages/services/CloudManagement";
-import DatabaseManagement from "./pages/services/DatabaseManagement";
-import AWSServices from "./pages/services/AWSServices";
+import WebsiteDesigning from "./pages/Services/WebsiteDesigning";
+import CRMDevelopment from "./pages/Services/CRMDevelopment";
+import AppDevelopment from "./pages/Services/AppDevelopment";
+import CloudManagement from "./pages/Services/CloudManagement";
+import DatabaseManagement from "./pages/Services/DatabaseManagement";
+import AWSServices from "./pages/Services/AWSServices";
 
 import WhyChooseUs from "./pages/WhyChooseUs";
 import Contact from "./pages/Contact";
@@ -26,12 +26,12 @@ function App() {
         <Route path="/about" element={<About />} />
         {/* Services */}
         {/* <Route path="/services" element={<Services />} /> */}
-        <Route path="/services/website-designing" element={<WebsiteDesigning />} />
-        <Route path="/services/crm-development" element={<CRMDevelopment />} />
-        <Route path="/services/Application-Development" element={<AppDevelopment />} />
-        <Route path="/services/cloud-management" element={<CloudManagement />} />
-        <Route path="/services/database-management" element={<DatabaseManagement />} />
-        <Route path="/services/aws-services" element={<AWSServices />} />
+        <Route path="/Services/website-designing" element={<WebsiteDesigning />} />
+        <Route path="/Services/crm-development" element={<CRMDevelopment />} />
+        <Route path="/Services/Application-Development" element={<AppDevelopment />} />
+        <Route path="/Services/cloud-management" element={<CloudManagement />} />
+        <Route path="/Services/database-management" element={<DatabaseManagement />} />
+        <Route path="/Services/aws-services" element={<AWSServices />} />
 
         <Route path="/why-choose-us" element={<WhyChooseUs />} />
         <Route path="/pricing" element={<Pricing />} />
